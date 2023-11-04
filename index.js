1d44507ab050f83be822752e7a59ddbc6e1a2de3
cuongdv <cuongdvdev03@gmail.com>
2023-11-04 15:04:57 +0700

    home

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(express.static('/public/dist/img'));
+app.use(express.static('views'));
 
 
 const options = {
