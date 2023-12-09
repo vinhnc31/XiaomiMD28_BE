b00e83dc31bf2100bb07cc852058906eac690e1c
vinhnc31 <congvinh3110@gmail.com>
2023-12-09 18:52:38 +0700

    fig nỗi account end update notifi

--- routers/notify.router.js
+++ routers/notify.router.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const notifyController = require("../controllers/notify.controller");
-router.get("/notification/:AccountId", notifyController.getNotify);
+const authMidleWare = require("../middleWare/auth.middlewere");
+router.get(
+  "/notification/:AccountId",
+  authMidleWare.apiAuth,
+  notifyController.getNotify
+);
 router.post("/notification/:AccountId", notifyController.sendMessage);
 router.post("/notification", notifyController.sendMessageAll);
 module.exports = router;
